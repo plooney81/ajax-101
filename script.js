3a78cb0cea8411d99ff257c87c40f4050837cd6a
Pete Looney <petelooney81@gmail.com>
2020-09-15 21:54:04 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Traditional AJAX request
+// const request = new XMLHttpRequest();
+// request.onreadystatechange = function () {
+//     if(this.readyState === 4 && this.status === 200){
+//         console.log(this.responseText);
+//     }
+// };
+// request.open("GET", "./sample.txt");
+// request.send();
+
+// Fetch API method
+fetch("./sample.txt").then((response)=>{
+    console.log(response);
+})
